b8937babb22a935a040566e439fc7234aaa8da05
mdbinger <mdbinger@gmail.com>
2022-08-04 17:14:40 -0700

    Completed challenge that calculates the difference in days between two datetime dates

---
+++ Python_examples/Easy/Days_between_dates.py
@@ -0,0 +1,28 @@
+# How Many Days Between Two Dates
+# Create a function get_days that takes two dates and returns the number of days between the first and second date.
+
+# Examples
+# get_days(
+#   datetime.date(2019, 6, 14),  # June 14, 2019
+#   datetime.date(2019, 6, 20)  # June 20, 2019
+# ) ➞ 6
+
+# get_days(
+#   datetime.date(2018, 12, 29),  # December 29, 2018
+#   datetime.date(2019, 1, 1)  # January 1, 2019
+# ) ➞ 3
+# Dates may not all be in the same month/year.
+
+# get_days(
+#   datetime.date(2020, 5, 24),
+#   datetime.date(2019, 5, 24))
+# ) ➞ -366
+# Backwards dates should return a negative change.
+
+# MY SOLUTION
+
+import datetime
+
+def get_days(date1, date2):
+	date_difference = date2 - date1
+	return date_difference.days
